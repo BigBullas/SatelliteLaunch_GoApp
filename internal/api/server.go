d28d138fbb31b8c1486f246b5c4d3177eca8f794
Alik Nigmatullin <NigAlik020503@yandex.ru>
2023-09-16 21:38:24 +0300

    created structure of project

---
+++ internal/api/server.go
@@ -0,0 +1,36 @@
+package api
+
+import (
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+)
+
+func StartServer() {
+	log.Println("Server start up")
+
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	r.LoadHTMLGlob("templates/*")
+
+	r.GET("/home", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.tmpl", gin.H{
+			"title": "Main website",
+		})
+	})
+
+	r.Static("/style", "./style")
+	r.Static("/image", "./resources")
+
+	err := r.Run()
+	if err != nil {
+		return
+	}
+
+	log.Println("Server down")
+}
