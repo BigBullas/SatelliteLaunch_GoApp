5bdc3b7bcfb4e675eaeefffd190c2c696ce75008
Alik Nigmatullin <NigAlik020503@yandex.ru>
2023-12-25 18:30:15 +0300

    fixed detailed_desc and added pgAmin connection to db

--- sql/init.sql
+++ sql/init.sql
@@ -28,17 +28,17 @@ create table if not exists rocket_flights
 
 create table if not exists flight_requests
 (
-    request_id          integer not null 
-                        constraint request_pk primary key,
-    is_available        boolean,
-    img_url             TEXT,
-    title               varchar(100),
-    load_capacity       float,
-    description         TEXT,
-    detailed_desc       TEXT,
-    desired_price       float,
-    flight_date_start   timestamp,
-    flight_date_end     timestamp
+    request_id          	integer not null 
+                        	constraint request_pk primary key,
+    is_available        	boolean,
+    img_url             	TEXT,
+    title               	varchar(100),
+    load_capacity       	float,
+    description         	TEXT,
+    detailed_desc		    TEXT,
+    desired_price       	float,
+    flight_date_start   	timestamp,
+    flight_date_end     	timestamp
 );
 
 create table if not exists flights_flight_requests
